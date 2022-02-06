020f049e9023b4877c8d89b9364884b8562966ef
Agilan Gunalan <aguquantum@gmail.com>
2022-02-05 19:21:44 -0500

    auto stuff

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -2,13 +2,24 @@ package frc.robot.subsystems;
 
 import frc.robot.RobotConstants;
 import frc.robot.subsystems.ColorSensor;
+
+import java.util.ArrayList;
+
 import com.kauailabs.navx.frc.*;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -78,6 +89,31 @@ public class DriveSubsystem extends EntechSubsystem {
         robotDrive.arcadeDrive(forward, rotation);
         feedWatchDog();
     }
+
+    public Trajectory generateTrajectory() {
+
+        var currentPos = new Pose2d(Units.feetToMeters(0), Units.feetToMeters(0),
+            Rotation2d.fromDegrees(0));
+        var endpoint = new Pose2d(Units.feetToMeters(3), Units.feetToMeters(4),
+            Rotation2d.fromDegrees(45));
+    
+        var interiorWaypoints = new ArrayList<Translation2d>();
+        interiorWaypoints.add(new Translation2d(Units.feetToMeters(1), Units.feetToMeters(2)));
+        interiorWaypoints.add(new Translation2d(Units.feetToMeters(2), Units.feetToMeters(3)));
+    
+        TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(2), Units.feetToMeters(.5));
+        config.setReversed(false);
+    
+        var trajectory = TrajectoryGenerator.generateTrajectory(
+            currentPos,
+            interiorWaypoints,
+            endpoint,
+            config);
+            
+            return trajectory;
+      }
+
+
 
 
     public double getAngle() {
