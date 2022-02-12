74ccf1601f8b6710ee04210fd1bef474983f0d7c
John-Molfetner <jtmolfenter07@gmail.com>
2022-02-12 15:28:01 -0800

    Added turn modified the drive straights

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -23,6 +23,7 @@ public final class RobotConstants {
     public interface DRIVER_STICK {
         public static final int DRIVESTRAIGHT = 1;
         public static final int DRIVESTRAIGHTGYRO = 2;
+        public static final int TURNBYANGLE = 3;
     }
     public interface CAN {
         public static final int FRONT_LEFT_MOTOR = 3;
--- src/main/java/frc/robot/commands/DriveStraightGyroCommand.java
+++ src/main/java/frc/robot/commands/DriveStraightGyroCommand.java
@@ -1,40 +1,42 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class DriveStraightGyroCommand extends EntechCommandBase {
     private final DriveSubsystem m_drive;
-    private final double m_distance;
     private final double m_speed;
     private final double m_initalAngle;
-    private double m_currentDistance;
+    private PIDController m_PID;
 
     public DriveStraightGyroCommand(DriveSubsystem drive, double inches, double speed){
         super(drive);
-        m_distance = inches;
         m_speed = speed;
         m_drive = drive;
-        m_currentDistance = 0;
+        m_PID = new PIDController(0.015, 0, 0);
         m_initalAngle = m_drive.getAngle();
     }
-    
 
     public void initialize(){
     m_drive.arcadeDrive(0, 0);
     m_drive.resetEncoders();
+    m_PID.setSetpoint(0);
+    m_PID.setTolerance(5);
     }
 
     public void execute(){
         double currentAngle = m_drive.getAngle();
-        double adjustment = 0.025 * ( m_initalAngle - currentAngle);
+        double adjustment = m_PID.calculate( m_initalAngle - currentAngle);
         m_drive.arcadeDrive(m_speed, adjustment);
-        m_currentDistance = (m_drive.getLeftDistance() + m_drive.getRightDistance()) / 2;
     }
 
     public boolean isFinished(){
-        if ( m_currentDistance > m_distance) {
+        if ( m_PID.atSetpoint()){
             return true;
         }
         return false;
+    }
+    public void end(boolean interrupted){
+        m_drive.arcadeDrive(0, 0);
     }
 }
