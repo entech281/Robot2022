b4f23363cf28dacd49e532d285a1b080d872fcf6
John-Molfetner <jtmolfenter07@gmail.com>
2022-02-14 20:03:03 -0800

    start

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -26,10 +26,11 @@ public final class RobotConstants {
         public static final int TURNBYANGLE = 3;
     }
     public interface CAN {
-        public static final int FRONT_LEFT_MOTOR = 3;
-        public static final int REAR_LEFT_MOTOR = 4;
         public static final int FRONT_RIGHT_MOTOR = 1;
         public static final int REAR_RIGHT_MOTOR = 2;
+        public static final int FRONT_LEFT_MOTOR = 3;
+        public static final int REAR_LEFT_MOTOR = 4;
+        public static final int BELT_MOTOR = 5;
     }
     public interface DIGITAL_IO {
     }
---
+++ src/main/java/frc/robot/subsystems/BeltSubsystem.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import frc.robot.RobotConstants;
+
+public class BeltSubsystem extends EntechSubsystem {
+private VictorSPX m_motor;
+
+  public enum BeltMode{
+  stop, in, out, auto
+}
+  private BeltMode currentMode = BeltMode.stop;
+
+  public BeltSubsystem(){
+  }
+
+  // Entech does all the creation work in the initialize method
+  @Override
+  public void initialize(){
+    m_motor = new VictorSPX(RobotConstants.CAN.BELT_MOTOR);
+    // Create the internal objects here
+  }
+
+  @Override
+  public void periodic(){
+    if (currentMode == BeltMode.stop){
+      m_motor.set(ControlMode.PercentOutput,0.0);
+    } else if (currentMode == BeltMode.in){
+      m_motor.set(ControlMode.PercentOutput, 1.0);
+    } else if (currentMode == BeltMode.out){
+      m_motor.set(ControlMode.PercentOutput, -1.0);
+    }
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic(){
+    // This method will be called once per scheduler run during simulation
+  }
+}
