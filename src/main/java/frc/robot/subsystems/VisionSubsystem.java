018c3061011a29d3f9acdd306536686da3adeae5
Michael Andrews <mandrews@mandrews.org>
2022-02-27 21:05:59 -0500

    Try to fix initial connection between rio and rpi

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -10,7 +10,9 @@ public class VisionSubsystem extends EntechSubsystem {
     private NetworkTable table;
     private static NetworkTableEntry tableEntryLowerBound;
     private static NetworkTableEntry tableEntryUpperBound;
-    private int counter;
+    private int rpi_counter;
+    private int rpi_dead_counter;
+    private int rio_counter;
     private int x_pos;
     private int y_pos;
     private Boolean isBallFound;
@@ -21,6 +23,9 @@ public class VisionSubsystem extends EntechSubsystem {
 
     @Override
     public void initialize(){
+        rio_counter = 0;
+        rpi_counter = -1;
+        rpi_dead_counter = 0;
         table = NetworkTableInstance.getDefault().getTable("Vision");
         tableEntryLowerBound = table.getEntry("HSVValuesLowerBound");
         tableEntryUpperBound = table.getEntry("HSVValuesUpperBound");
@@ -33,7 +38,6 @@ public class VisionSubsystem extends EntechSubsystem {
             Number[] arrUpperBound = {180, 255, 255};
             tableEntryLowerBound.forceSetNumberArray(arrLowerBound);  
             tableEntryUpperBound.forceSetNumberArray(arrUpperBound);
-
         }
         if (DriverStation.getAlliance() == DriverStation.Alliance.Blue){
             Number[] arrLowerBound = {100, 100, 0};
@@ -47,20 +51,31 @@ public class VisionSubsystem extends EntechSubsystem {
             tableEntryLowerBound.forceSetNumberArray(arrLowerBound);  
             tableEntryUpperBound.forceSetNumberArray(arrUpperBound);            
         }
-
         
     }
 
     @Override
     public void periodic(){
 
+        // send rio information to the network tables
+        table.getEntry("rio_counter").setNumber(rio_counter);
+        rio_counter += 1;
         publishHSV();
-        counter = table.getEntry("Counter").getNumber(0.0).intValue();
+
+        // get the counter and see if the rpi is alive
+        int counter = table.getEntry("rpi_counter").getNumber(0.0).intValue();
+        if (counter > rpi_counter) {
+            rpi_counter = counter;
+            rpi_dead_counter = 0;
+        } else {
+            rpi_dead_counter += 1;
+        }
         x_pos = table.getEntry("x").getNumber(0).intValue();
         y_pos = table.getEntry("y").getNumber(0).intValue();
         isBallFound = table.getEntry("isBallFound").getBoolean(false);
 
-        logger.log("Counter", counter);
+        logger.log("rpi counter", rpi_counter);
+        logger.log("rpi dead counter", rpi_dead_counter);
         logger.log("Ball Lateral Offset", x_pos);
         logger.log("Ball Vertical Offset", y_pos);
         logger.log("Vision Ball Found?", isBallFound);        
@@ -69,6 +84,13 @@ public class VisionSubsystem extends EntechSubsystem {
 
     public Boolean isBallFound() {
         return isBallFound;
+    }
+
+    public Boolean isRPiAlive() {
+        if (rpi_dead_counter > 50) {
+            return false;
+        }
+        return true;
     }
 
     public int getBallX() {
--- src/main/python/vision.py
+++ src/main/python/vision.py
@@ -10,9 +10,16 @@ print("waiting 30 seconds")
 time.sleep(30)
 print("wait complete")
 
-ntinst = NetworkTablesInstance.getDefault()
-ntinst.startClientTeam(281)
-ntinst.startDSClient()
+if False:
+    ntinst = NetworkTablesInstance.getDefault()
+    ntinst.startClientTeam(281)
+    ntinst.startDSClient()
+else:
+    # NetworkTables.setTeam(281)
+    # NetworkTables.setClientMode()
+    NetworkTables.setUpdateRate(0.020)
+    # NetworkTables.initialize(server="roborio-281-frc.local")
+    NetworkTables.initialize(server="10.2.81.2")
 
 vision_nt = NetworkTables.getTable('Vision')
 
@@ -29,12 +36,10 @@ sink = cs.getVideo()
 outputSource = cs.putVideo("Feed", 320, 240)
 
 img = np.zeros(shape = (240, 320, 3), dtype = np.uint8)
-lower_bound = vision_nt.getNumberArray("HSVValuesLowerBound", (0,0,0))
-upper_bound = vision_nt.getNumberArray("HSVValuesUpperBound", (0,0,0))
+kernel = np.ones((3,3), np.uint8)
 
-counter = 0
+rpi_counter = 0
 while True:
-    counter += 1
     x_rel = 0
     y_rel = 0
     isBallFound = False
@@ -43,51 +48,48 @@ while True:
 
     lower_bound = vision_nt.getNumberArray("HSVValuesLowerBound", (0,0,0))
     upper_bound = vision_nt.getNumberArray("HSVValuesUpperBound", (0,0,0))
-    if lower_bound[0] == 0:
-        continue
 
     if time == 0: # There is an error
         outputSource.notifyError(sink.getError())
         continue
 
+    rpi_counter += 1
     blurred = cv2.GaussianBlur(input_img, (11,11), 0)
     hsv_img = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
     binary_img = cv2.inRange(hsv_img, lower_bound, upper_bound)
-    binary_img = cv2.erode(binary_img, None, iterations=2)
-    binary_img = cv2.dilate(binary_img, None, iterations=2)
-
-    kernel = np.ones((3,3), np.uint8)
-    binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
+    binary_img = cv2.erode(binary_img, kernel, iterations=2)
+    binary_img = cv2.dilate(binary_img, kernel, iterations=2)
+    # binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
+    
     _, contours, _ = cv2.findContours(binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Mandrews
-    #  - draw a cross on the ball found on the imput image and send that to the drive station (can be done later)
-
+    # Find the best candidate contour
     indx = -1
     indx_ratio = 100.0
     for i in range(len(contours)):
         contour = contours[i]
-        if cv2.contourArea(contour) < 15:
+        if cv2.contourArea(contour) < 10:
             continue
         x,y,w,h = cv2.boundingRect(contour)
         if (w>2) and (h>2):
             ratio = float(max(w,h))/float(min(w,h))
             if ratio < indx_ratio:
+                indx_ratio = ratio
                 indx = i
 
     if indx > -1:
         contour = contours[indx]
         isBallFound = True
-        cv2.drawContours(binary_img, contour, -1, color = (255, 255, 255), thickness = -1)
+        # cv2.drawContours(binary_img, contour, -1, color = (255, 255, 255), thickness = -1)
         ball = cv2.minEnclosingCircle(contour)
         center, size = ball
         x, y = center
-        cv2.circle(output_img, (int(x), int(y)), int(size), (255,255,255), 2)
         x_rel = 160 - x #Positive -> left hand side
         y_rel = 120 - y #Positive -> top half of the screen
+        cv2.circle(output_img, (int(x), int(y)), int(size), (255,255,255), 2)
 
-    vision_nt.putNumber("Counter", counter)
+    vision_nt.putNumber("rpi_counter", rpi_counter)
     vision_nt.putBoolean("isBallFound", isBallFound)
     vision_nt.putNumber("x", x_rel)
     vision_nt.putNumber("y", y_rel)
