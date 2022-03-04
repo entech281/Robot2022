68fd3f0ef20224a2eff030c5b08b9f49662e90cc
Entech student <student@entech281.org>
2022-03-03 20:58:55 -0400

    Intake commands implemented

---
+++ src/main/java/frc/robot/commands/IntakeOutCommand.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.IntakeSubsystem;
+
+/** An example command that uses an example subsystem. */
+public class IntakeOutCommand extends EntechCommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final IntakeSubsystem m_subsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public IntakeOutCommand(IntakeSubsystem subsystem) {
+      super(subsystem);
+      m_subsystem = subsystem;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  @Override
+  public void execute() {
+      m_subsystem.rollersOut();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+      return true;
+  }
+
+  // Returns true if this command should run when robot is disabled.
+  @Override
+  public boolean runsWhenDisabled() {
+      return false;
+  }
+}
