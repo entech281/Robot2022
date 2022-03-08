cdb9f2f0e7ec416b9c29a2adac981f7ada578868
Michael Andrews <mandrews@mandrews.org>
2022-03-07 22:28:41 -0500

    Add missing commandFactory methods.  Operator panel implemented

--- src/main/java/frc/robot/commands/CommandFactory.java
+++ src/main/java/frc/robot/commands/CommandFactory.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.SubsystemManager;
@@ -40,6 +41,16 @@ public class CommandFactory {
     public Command getAutoBeltCommand(){
         return new AutoBeltCommand(sm.getBeltSubsystem(),sm.getColorSensorSubsystem());
     }
+    public Command getShootBallCommand(){
+        return new SequentialCommandGroup(
+            getBeltOutCommand(),
+            new WaitCommand(0.1),
+            getBeltInCommand(),
+            new WaitCommand(1.25),
+            getBeltStopCommand()
+        );
+    }
+
     public Command getIntakeInCommand(){
         return new IntakeInCommand(sm.getIntakeSubsystem());
     }
@@ -49,6 +60,33 @@ public class CommandFactory {
     public Command getIntakeStopCommand(){
         return new IntakeStopCommand(sm.getIntakeSubsystem());
     }
+    public Command getIntakeArmDownCommand(){
+        return new IntakeArmDownCommand(sm.getIntakeSubsystem());
+    }
+    public Command getIntakeArmUpCommand(){
+        return new IntakeArmUpCommand(sm.getIntakeSubsystem());
+    }
+    public Command getIntakeArmNudgeDownCommand(){
+        return new IntakeArmNudgeDownCommand(sm.getIntakeSubsystem());
+    }
+    public Command getIntakeArmNudgeUpCommand(){
+        return new IntakeArmNudgeUpCommand(sm.getIntakeSubsystem());
+    }
+    public Command getIntakeDeployCommand() {
+        return new ParallelCommandGroup(
+            getAutoBeltCommand(),
+            getIntakeInCommand(),
+            getIntakeArmDownCommand()
+        );
+    }
+    public Command getIntakeRetractCommand() {
+        return new ParallelCommandGroup(
+            getBeltStopCommand(),
+            getIntakeStopCommand(),
+            getIntakeArmUpCommand()
+        );
+    }
+
     public Command getHookUpCommand(){
         return new HookUpCommand(sm.getHookSubsystem());
     }
@@ -58,6 +96,7 @@ public class CommandFactory {
     public Command getHookStopCommand(){
         return new HookStopCommand(sm.getHookSubsystem());
     }
+
     public Command getVisionDriveCommand(){
         return new VisionDriveCommand(sm.getDriveSubsystem(), sm.getVisionSubsytem());
     }
@@ -78,13 +117,6 @@ public class CommandFactory {
     }
     public Command getTurnUntillBallSeenCommand() {
         return new TurnUntilBallSeenCommand(sm.getDriveSubsystem(),sm.getVisionSubsytem());
-    }
-    public Command getShootBallCommand(){
-        return new SequentialCommandGroup(
-            getBeltInCommand(),
-            new WaitCommand(1.0),
-            getBeltStopCommand()
-        );
     }
 
     public Command getAutonomousCommand(int choice){
--- src/main/java/frc/robot/subsystems/HookSubsystem.java
+++ src/main/java/frc/robot/subsystems/HookSubsystem.java
@@ -5,16 +5,15 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.RobotConstants;
 
 public class HookSubsystem extends EntechSubsystem {
   private TalonSRX m_motor;
-  private DigitalInput m_UpLimit;
-  private DigitalInput m_DownLimit;
   private final double motorSpeed = 1.0;
   public enum HookMode{
     idle, up, down
@@ -30,11 +29,10 @@ public class HookSubsystem extends EntechSubsystem {
     // Create the internal objects here
     m_motor = new TalonSRX(RobotConstants.CAN.HOOK_MOTOR);
     m_motor.setNeutralMode(NeutralMode.Brake);
-    // m_motor.enableCurrentLimit(true);
-    // m_motor.configContinuousCurrentLimit(40);
-    // m_motor.configPeakCurrentLimit(0);
-    m_UpLimit = new DigitalInput(RobotConstants.DIGITAL_IO.HOOK_UP_LIMIT);
-    m_DownLimit = new DigitalInput(RobotConstants.DIGITAL_IO.HOOK_DOWN_LIMIT);
+    // Configure limit switches for normally open
+    m_motor.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
+    m_motor.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
+    m_motor.overrideLimitSwitchesEnable(true);
   }
 
   public boolean isLimitSwitchHit(){
@@ -43,20 +41,17 @@ public class HookSubsystem extends EntechSubsystem {
 
   @Override
   public void periodic() {
-    // TODO: the checks against the limit switches below are reversed since there are currently
-    //   no limit siwtches on the robot and they read true by default.  Fix this or remove limit switches.
-    if ((currentMode == HookMode.up) && (m_UpLimit.get() == false)) {
-      currentMode = HookMode.idle;
-    }
-    if ((currentMode == HookMode.down) && (m_DownLimit.get() == false)) {
+    // check limit switches -- either Fwd or Rev
+    boolean downLimitSwitchHit = m_motor.getSensorCollection().isFwdLimitSwitchClosed();
+    if ((currentMode == HookMode.down) && downLimitSwitchHit) {
       currentMode = HookMode.idle;
     }
     if (currentMode == HookMode.idle){
       m_motor.set(ControlMode.PercentOutput, 0.0);
     } else if (currentMode == HookMode.up) {
-      m_motor.set(ControlMode.PercentOutput, motorSpeed);
-    } else if (currentMode == HookMode.down) {
       m_motor.set(ControlMode.PercentOutput, -motorSpeed);
+    } else if (currentMode == HookMode.down) {
+      m_motor.set(ControlMode.PercentOutput, motorSpeed);
     }
     logger.log("hook current",m_motor.getSupplyCurrent());
     logger.log("hook limit switch:", isLimitSwitchHit());
